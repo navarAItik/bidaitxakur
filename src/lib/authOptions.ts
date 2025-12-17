c6998eda794c41df556c28fa854b3bb56f3f569b
Mikel <mapestesar@educacion.navarra.es>
2025-12-17 22:58:58 +0100

    probando

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -1,5 +1,4 @@
 import type { NextAuthOptions } from 'next-auth';
-import EmailProvider from 'next-auth/providers/email';
 import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions: NextAuthOptions = {
@@ -7,10 +6,6 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-    }),
-    EmailProvider({
-      server: process.env.EMAIL_SERVER ?? '',
-      from: process.env.EMAIL_FROM ?? 'noreply@huellasdelnorte.com',
     }),
   ],
   callbacks: {
